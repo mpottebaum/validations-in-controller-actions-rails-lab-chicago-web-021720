c4e6e31a2ee9f02f80185cfa365aed5097694862
mpottebaum <mpottebaum@gmail.com>
2020-03-18 11:22:31 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    @post.assign_attributes(post_params)
 
-    redirect_to post_path(@post)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
